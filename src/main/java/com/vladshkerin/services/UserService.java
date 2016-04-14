a9f01444f4cdc49f9ad3c6f4965ee1c6735fc083
Vladimir Shkerin <vladshkerin@gmail.com>
2016-04-14 15:19:02 +0500

    Add method delete() to class UserService

--- src/main/java/com/vladshkerin/services/UserService.java
+++ src/main/java/com/vladshkerin/services/UserService.java
@@ -20,14 +20,18 @@ public class UserService {
     private final List<User> users = new CopyOnWriteArrayList<>();
 
     private UserService() {
-        GregorianCalendar cal1 = new GregorianCalendar(1985, 1, 2);
-        GregorianCalendar cal2 = new GregorianCalendar(1990, 5, 6);
-        String[] children1 = new String[]{"Ivan", "Oleg"};
-        String[] children2 = new String[]{"Maria", "Eva"};
+        GregorianCalendar calendar1 = new GregorianCalendar(1985, 1, 2);
+        GregorianCalendar calendar2 = new GregorianCalendar(1990, 3, 4);
+        GregorianCalendar calendar3 = new GregorianCalendar(1995, 5, 6);
+        String[] children1 = new String[]{"Ivan"};
+        String[] children2 = new String[]{"Sviatoslav", "Eva"};
+        String[] children3 = new String[]{"Maria", "Sergio", "Irina"};
 
-        users.add(new User("00001", "Petr", 176.6f, cal1, children1));
-        users.add(new User("00002", "Make", 150f, cal2, children2));
-        users.add(new User("00003", "Olga"));
+        users.add(new User("00001", "Petr", 176.6f, calendar1, children1));
+        users.add(new User("00002", "Erik", null, calendar2, children2));
+        users.add(new User("00003", "Make", 160f, null, children3));
+        users.add(new User("00004", "Nikita", 145.12f, calendar3, null));
+        users.add(new User("00005", "Olga"));
     }
 
     public static UserService getInstance() {
@@ -49,5 +53,16 @@ public class UserService {
             }
         }
         throw new NotFoundUser("User not fount to \"id\"" + id);
+    }
+
+    public void delete(String id) {
+        if (!id.isEmpty()) {
+            try {
+                users.remove(this.get(id));
+            } catch (NotFoundUser e) {
+                //TODO add out to log
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
