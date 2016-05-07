aa61952474b1438781a7499a9e9731f8a1c5cbad
Vladimir Shkerin <vladshkerin@gmail.com>
2016-05-07 09:49:20 +0500

    Modify the "doPost" method to validate the form and added the method "setSessionAttribute"

--- src/main/java/com/vladshkerin/servlets/UserAddServlet.java
+++ src/main/java/com/vladshkerin/servlets/UserAddServlet.java
@@ -7,7 +7,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * TODO: comment
@@ -19,21 +22,36 @@ public class UserAddServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("id");
-        if (id != null && !id.isEmpty()) {
-            String name = req.getParameter("name");
-            String growth = req.getParameter("growth");
-            String birthDay = req.getParameter("birthDay");
-            String email = req.getParameter("email");
-            String[] children = req.getParameterValues("children");
+        Map<String, String> userPropertiesMap = new HashMap<>();
+        userPropertiesMap.put("id", req.getParameter("id").trim());
+        userPropertiesMap.put("name", req.getParameter("name").trim());
+        userPropertiesMap.put("growth", req.getParameter("growth").trim());
+        userPropertiesMap.put("birthDay", req.getParameter("birthDay").trim());
+        userPropertiesMap.put("email", req.getParameter("email").trim());
 
-            User user = new User(id.trim(), name.trim());
-            user.setGrowth(growth.trim());
-            user.setBirthDay(birthDay.trim());
-            user.setEmail(email);
-            user.setChildren(children);
+        HttpSession session = req.getSession();
+        String errorValues = UserService.getInstance().validateForm(userPropertiesMap);
+        if (errorValues.isEmpty()) {
+            String name = userPropertiesMap.get("name");
+            User user = new User(name);
+            user.setGrowth(userPropertiesMap.get("growth"));
+            user.setBirthDay(userPropertiesMap.get("birthDay"));
+            user.setEmail(userPropertiesMap.get("email"));
+            user.setChildren(req.getParameter("children").trim());
             UserService.getInstance().add(user);
+
+            setSessionAttribute("message", "The user \"" + name + "\" is added.", session);
+        } else {
+            String message = "Incorrect input values: " + errorValues + " !";
+            setSessionAttribute("message", message, session);
+            // alert ????
         }
         resp.sendRedirect(String.format("%s/views/UserAdd.jsp", req.getContextPath()));
+    }
+
+    private void setSessionAttribute(String name, Object value, HttpSession session) {
+        synchronized (session) {
+            session.setAttribute(name, value);
+        }
     }
 }
