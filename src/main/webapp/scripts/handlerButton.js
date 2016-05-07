44d09950fc8f83c8490a255594c5173edfde1de4
Vladimir Shkerin <vladshkerin@gmail.com>
2016-05-07 09:38:44 +0500

    Add function validateForm()

--- src/main/webapp/scripts/handlerButton.js
+++ src/main/webapp/scripts/handlerButton.js
@@ -3,23 +3,35 @@
  * Created by vlad on 14.04.16.
  */
 
-window.onload = function () {
-    var divMessage = document.getElementById("message");
+function validateForm() {
+    var fieldValues = [
+        {name: "Id", value: document.getElementsByName("id")[0].value},
+        {name: "Name", value: document.getElementsByName("name")[0].value},
+        {name: "Growth", value: document.getElementsByName("growth")[0].value},
+        {name: "Birth Day", value: document.getElementsByName("birthDay")[0].value},
+        {name: "Email", value: document.getElementsByName("email")[0].value}
+    ];
 
-    function setAttributesForTag(tag, text) {
-        tag.innerHTML = text;
-        tag.setAttribute("style", "color: red; text-align: center; margin-top: 10px;");
+    var errorValues = [];
+    for (var i = 0; i < fieldValues.length; i++) {
+        if (fieldValues[i].value.trim() === "") {
+            errorValues[errorValues.length] = fieldValues[i].name;
+        } else if (fieldValues[i].name === "Growth") {
+            if (fieldValues[i].value < 1 || fieldValues[i] > 200) {
+                errorValues[errorValues.length] = fieldValues[i].name;
+            }
+        }
     }
 
-    if (divMessage.innerHTML !== "") {
-        setAttributesForTag(divMessage, divMessage.innerHTML);
+    if (errorValues.length > 0) {
+        var testMessage = "Incorrect input values: " + errorValues + " !";
+        var message = document.getElementById("message");
+        message.innerHTML = testMessage;
+        message.setAttribute("style", "color: red");
+        // alert(testMessage);
+
+        return false;
     }
 
-    document.getElementById("buttonSave").onclick = function () {
-        setAttributesForTag(divMessage, "The changes are saved.");
-    };
-
-    document.getElementById("buttonDelete").onclick = function () {
-        setAttributesForTag(divMessage, "Deleted user.");
-    };
-};
+    return true;
+}
