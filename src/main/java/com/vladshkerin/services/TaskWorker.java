1b35681f0b7af2963f8c1d23ccf0f5e33ad4a7e5
Vladimir Shkerin <vladshkerin@gmail.com>
2016-06-01 17:28:00 +0500

    Add class "TaskWorker.java" to run a single thread.

---
+++ src/main/java/com/vladshkerin/services/TaskWorker.java
@@ -0,0 +1,31 @@
+package com.vladshkerin.services;
+
+import com.vladshkerin.enums.Operation;
+
+/**
+ * The class to run a single operation.
+ *
+ * @author Vladimir Shkerin
+ * @since 01.06.2016
+ */
+public class TaskWorker implements Runnable {
+
+    private Operation operation;
+
+    public TaskWorker(Operation operation) {
+        this.operation = operation;
+    }
+
+    @Override
+    public void run() {
+        if (Operation.LOAD_USERS.equals(operation)) {
+            UserService.getInstance().loadFile();
+        } else if (Operation.LOAD_ITEMS.equals(operation)) {
+            ItemService.getInstance().loadFile();
+        } else if (Operation.SAVE_USERS.equals(operation)) {
+            UserService.getInstance().saveFile();
+        } else if (Operation.SAVE_ITEMS.equals(operation)) {
+            ItemService.getInstance().saveFile();
+        }
+    }
+}
