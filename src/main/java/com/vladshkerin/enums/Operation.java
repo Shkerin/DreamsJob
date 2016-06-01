43321870426f21bf8cac460e13bda374c248522c
Vladimir Shkerin <vladshkerin@gmail.com>
2016-06-01 17:29:12 +0500

    Add enum "Operation" for stream operations on the data.

---
+++ src/main/java/com/vladshkerin/enums/Operation.java
@@ -0,0 +1,14 @@
+package com.vladshkerin.enums;
+
+/**
+ * The enum for stream operations.
+ *
+ * @author Vladimir Shkerin
+ * @since 01.06.2016
+ */
+public enum Operation {
+    SAVE_USERS,
+    SAVE_ITEMS,
+    LOAD_USERS,
+    LOAD_ITEMS
+}
