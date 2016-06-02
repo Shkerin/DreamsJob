dbc214ff6d396eef1dfa999958c500a1d50a5393
Vladimir Shkerin <vladshkerin@gmail.com>
2016-06-02 08:32:01 +0500

    Add servlet "ItemDeleteServlet.java" for delete item.

---
+++ src/main/java/com/vladshkerin/servlets/ItemDeleteServlet.java
@@ -0,0 +1,32 @@
+package com.vladshkerin.servlets;
+
+import com.vladshkerin.services.ItemService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * The servlet class for delete item.
+ *
+ * @author Vladimir Shkerin
+ * @since 01.06.2016
+ */
+public class ItemDeleteServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        if (id != null && !id.isEmpty()) {
+            ItemService.getInstance().delete(Long.valueOf(id));
+        }
+        req.getRequestDispatcher("navigation?page=items").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
