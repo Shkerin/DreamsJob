ef745855954e94c789f87d7148f99263f7797650
Vladimir Shkerin <vladshkerin@gmail.com>
2016-06-07 22:01:21 +0500

    Add filter "AuthenticationFilter"

---
+++ src/main/java/com/vladshkerin/servlets/AuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.vladshkerin.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * The class of the servlet for user authentication.
+ *
+ * @author vlad
+ * @since 06.06.2016
+ */
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Filter");
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpSession session = req.getSession();
+
+        if (req.getRequestURI().contains("login")) {
+            chain.doFilter(request, response);
+        } else if (session == null || session.getAttribute("CURRENT_USER") == null) {
+            request.getRequestDispatcher("/WEB-INF/views/Login.jsp").forward(request, response);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
